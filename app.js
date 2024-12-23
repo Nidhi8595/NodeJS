0acd720dd815a4144bd9df7162994f26bfb3a4a4
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-12-23 12:19:22 +0530

    understanding the global object

---
+++ app.js
@@ -0,0 +1,15 @@
+let name = "Node JS";
+let a = 5;
+let b = 10;
+let c = a + b;
+console.log(name);
+console.log(c);
+
+console.log(this);
+
+// at the global level in Node.js, it will log an empty object, indicating that this does not refer to the global object in this context.
+
+console.log(globalThis);
+
+// In browsers, global is equivalent to window .
+// In Node.js, globalThis is equivalent to global .
